babdb57d180605d65b01913df878449d9bbfd2f0
ZHam2012 <34145989+ZHam2012@users.noreply.github.com>
2019-02-15 15:47:06 +1100

    Hololens
    Hololens

---
+++ Assets/camcode.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camcode : MonoBehaviour
+{
+    public Camera ViveCamera;
+    public Camera OtherCamera;
+    public Transform Source;
+    public Transform Destination;
+
+    //public static Quaternion rotate;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraPositionInSourceSpace = Source.InverseTransformPoint(ViveCamera.transform.position);
+        Quaternion cameraRotationInSourceSpace = Quaternion.Inverse(Source.rotation) * ViveCamera.transform.rotation;
+
+        OtherCamera.transform.position = Destination.TransformPoint(cameraPositionInSourceSpace);
+        OtherCamera.transform.rotation = (Destination.rotation * cameraRotationInSourceSpace);
+        OtherCamera.transform.rotation = Quaternion.FromToRotation(Vector3.up, -Vector3.up);
+        //OtherCamera.transform.rotation = ViewScript.rotate;
+    }
+}
